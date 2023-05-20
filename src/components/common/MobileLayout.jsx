f3dd53486bb52e35920eb3089e6eb1b1652a0b4a
Chanwoo-Jeong <lordroad38@naver.com>
2023-05-21 04:14:40 +0900

    feat : MainButton Component

--- src/components/common/MobileLayout.jsx
+++ src/components/common/MobileLayout.jsx
@@ -8,7 +8,9 @@ function MobileLayout({ children }) {
 export default MobileLayout;
 
 const MobileLayoutWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 32rem;
   height: 100vh;
-  border: 2px solid black;
 `;
