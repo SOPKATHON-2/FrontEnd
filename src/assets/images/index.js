39676dc166402bf390db9c8b7762698cf316d9ed
lydiacho <sheecho0804@ewhain.net>
2023-05-21 07:11:09 +0900

    feat : 불 커지기 다 구현

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,13 +1,14 @@
-import ImgF1 from './img_f1.png';
-import ImgF2 from './img_f2.png';
-import ImgF3 from './img_f3.png';
-import ImgF4 from './img_f4.png';
-import ImgF5 from './img_f5.png';
-import ImgF6 from './img_f6.png';
-import ImgF7 from './img_f7.png';
-import ImgF8 from './img_f8.png';
-import ImgF9 from './img_f9.png';
-import ImgF10 from './img_f10.png';
+import ImgF1 from './img_f2.png';
+import ImgF2 from './img_f3.png';
+import ImgF3 from './img_f4.png';
+import ImgF4 from './img_f5.png';
+import ImgF5 from './img_f6.png';
+import ImgF6 from './img_f7.png';
+import ImgF7 from './img_f8.png';
+import ImgF8 from './img_f9.png';
+import ImgF9 from './img_f10.png';
+import ImgF10 from './img_f11.png';
+
 
 export const IMG_DATA = [
     ImgF1,
