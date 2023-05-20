a8d97533b49de80db11998171deaf398340e6ef2
lydiacho <sheecho0804@ewhain.net>
2023-05-21 03:45:29 +0900

    feat : Campfire 컴포넌트 기본적인 스타일 구조

--- src/pages/Campfire.jsx
+++ src/pages/Campfire.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import MobileLayout from '../components/common/MobileLayout';
+import { styled } from 'styled-components';
+import mainLogo from '../assets/images/mainLogo.png'
+
+
 function Campfire() {
   return (
-    <MobileLayout>Campfire</MobileLayout>
+    <MobileLayout>
+      <Header>
+        <span>1</span>/10
+      </Header>
+      <Title>
+      담배 메이트 초대해서<br/>불을 더 활활 지펴보자고
+      </Title>
+
+      <ImgWrapper>
+        <img src={mainLogo} alt="걍 png"/>
+      </ImgWrapper>
+
+      <button>맞담할 사람 구함 </button>
+
+    </MobileLayout>
   )
 }
 
 export default Campfire
+
+
+const Header = styled.header`
+  text-align: center;
+
+`
+
+const Title = styled.h1`
+  text-align: center;
+`
+
+const ImgWrapper = styled.section`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+`
